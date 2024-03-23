1722156892d18722bb6fee64d64e3ba8631c7aef
X_V.neer <xv.neer@gmail.com>
2024-03-23 12:04:01 +0200

    added video to contact page

--- src/app/contact-us/layout.tsx
+++ src/app/contact-us/layout.tsx
@@ -15,16 +15,16 @@ const Layout = ({ children }: Props) => {
     <section className=" min-h-screen">
       <Toaster />
       <div className="h-full">
-        <div className="flex h-full justify-center p-4 lg:gap-32 lg:p-0">
-          <div className="h-full w-full lg:w-2/3 ">
-            <div className="flex min-h-full  max-lg:justify-center lg:pr-20 ">
-              <Link href={"/"} className="absolute right-4 top-4 flex gap-2  ">
-                <span className="self-center whitespace-nowrap text-2xl font-semibold  text-secondary ">
-                  UGC
-                </span>
-                <img src={logo.src} className="h-9" alt="logo" />
-              </Link>
-              <div className="w-full max-w-[650px] pb-10  pt-24 lg:pt-40">
+        <div className="flex h-full items-center justify-center p-4 lg:gap-32 lg:p-0">
+          <Link href={"/"} className="absolute right-4 top-4 flex gap-2  ">
+            <span className="self-center whitespace-nowrap text-2xl font-semibold  text-secondary ">
+              UGC
+            </span>
+            <img src={logo.src} className="h-9" alt="logo" />
+          </Link>
+          <div className=" w-full lg:w-2/3 ">
+            <div className="flex  max-lg:justify-center lg:pr-20 ">
+              <div className="w-full max-w-[650px]  max-lg:py-20">
                 <FormProvider>{children}</FormProvider>
               </div>
             </div>
@@ -32,11 +32,15 @@ const Layout = ({ children }: Props) => {
           {/*  */}
           <div className=" relative hidden min-h-screen  bg-primary  max-lg:hidden lg:block lg:w-1/3  ">
             <div className=" absolute top-[140px] z-10  aspect-[3/4] w-full  max-w-sm translate-x-14 overflow-hidden rounded-xl xl:translate-x-32">
-              <img
-                src={blogger.src}
-                alt="place"
+              <video
+                autoPlay
+                muted
+                loop
                 className=" h-full w-full object-cover"
-              />
+                preload="none">
+                <source src="/video3.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
             </div>
             <div className="absolute inset-x-0 h-full overflow-hidden">
               <span className="absolute right-[34%] top-[3%] block aspect-square w-10 rounded-full border  border-secondary  "></span>
